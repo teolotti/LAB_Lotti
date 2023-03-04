a5ee50390ac124b9754ee934f7aa64570e24e8cb
teolo <matteo.lotti3@stud.unifi.it>
2023-03-04 12:41:01 +0100

    heap

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
+from linked_list import*
+from node import*
+from ord_linked_list import*
+from heap import*
+import numpy as num
+from timeit import default_timer as timer
+import matplotlib.pyplot as plt
+import sys
+
+sys.setrecursionlimit(100000)
+
 
 def main():
-
-
-
-if __name__ == '__main__':
-    main()
+    if __name__ == '__main__':
+        main()
 
 
